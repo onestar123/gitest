3ccd21325773a32befb78abb01fbbcefe3d99695
Schlake <767760477@qq.com>
2018-07-19 23:55:45 +0800

    first

---
+++ leetcode.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict1 = {}
+        for k, i in enumerate(nums):
+            dict1[i] = k
+            if target - i in dict1 and i is not target - i:
+                return [dict1[target - i], dict1[i]]
+        # nums = [2, 7, 11, 15]
+        # target = 9
+        nums = [4, 3, 5, 15]
+        target = 8
+        s = Solution()
+        print(s.twoSum(nums, target))
+
+
+        adfafefasf
